4ad509c147b1f7a643b6fea2143b490b0bbc326b
Robert Gonciarz <gonciarz@gmail.com>
2018-10-24 00:18:55 +0200

    storage support

--- src/main/java/meet/patient/ports/PatientBindings.java
+++ src/main/java/meet/patient/ports/PatientBindings.java
@@ -10,18 +10,9 @@ public interface PatientBindings {
     String PATIENT_CREATE_REQUESTS = "patient-create-requests";
     String PATIENT_CREATE_RESULTS = "patient-create-results";
 
-    String PATIENT_GET_REQUESTS = "patient-get-requests";
-    String PATIENT_GET_RESULTS = "patient-get-results";
-    
     @Input(PATIENT_CREATE_REQUESTS)
     SubscribableChannel createPatientRequests();
 
     @Output(PATIENT_CREATE_RESULTS)
     MessageChannel createPatientResults();
-
-    @Input(PATIENT_GET_REQUESTS)
-    SubscribableChannel findAllRequests();
-
-    @Output(PATIENT_GET_RESULTS)
-    MessageChannel findAllResults();
 }
